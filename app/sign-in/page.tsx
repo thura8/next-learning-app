1fca2a71559458f3c5f7d089157df50af959848e
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-05-27 17:00:16 +0630

    Setting up supabase and clerk

--- app/sign-in/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div className="text-3xl text-violet-600">Sign In</div>;
-};
-
-export default page;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import NavItems from "./NavItems";
 
 const Navbar = () => {
@@ -13,7 +14,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
